8c9f86afa52db8dcb7a57014f797c8bb353460d3
SsamDemon <sultan.samid@gmail.com>
2023-10-07 19:59:47 -0400

    everything is working now. Keep working.

--- backend-java/app/src/main/java/biz/readmylegal/backend/RequestHttp.java
+++ backend-java/app/src/main/java/biz/readmylegal/backend/RequestHttp.java
@@ -3,16 +3,11 @@ package biz.readmylegal.backend;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
-import com.google.gson.Gson;
-
-import io.reactivex.exceptions.Exceptions;
-import retrofit2.http.Body;
-import retrofit2.http.PATCH;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RequestHttp {
 
@@ -20,23 +15,53 @@ public class RequestHttp {
 
         Transcript transcript = new Transcript();
         transcript.setAudio_url("https://github.com/AssemblyAI-Examples/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3");
-       Gson gson = new Gson();
-       
+
+    
+        //convert transcript inot Json
+        ObjectMapper objectmapper = new ObjectMapper();
+
+        String json = objectmapper.writeValueAsString(transcript);
+         System.out.println("I am here Right NOW ");
+        //pring the Set audio Url
+        System.out.println(json);
 
 
+    //Poste Request
         HttpRequest postRequest = HttpRequest.newBuilder()
             .uri(new URI("https://api.assemblyai.com/v2/transcript"))
             .header("Authorization", "3dbe811b98354c81be4802a625657bc1")
             .POST(BodyPublishers.ofString("{\"audio_url\": \"https://github.com/AssemblyAI-Examples/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3\"}"))
-            .build();
+            .build(); 
 
         HttpClient httpClient = HttpClient.newHttpClient();
         
-       HttpResponse<String> repostResponse =  httpClient.send(postRequest,BodyHandlers.ofString());
+        HttpResponse<String> repostResponse = httpClient.send(postRequest,BodyHandlers.ofString());
 
-        System.out.println(repostResponse.body());
+        System.out.println("here is Jason Start "+ repostResponse.body());
+       //geting Id
+        transcript = objectmapper.readValue(repostResponse.body(), Transcript.class);
+
+        System.out.println("Printing ID = "+transcript.getId());
 
 
+    //get call.
+    //covert jsyn into transcript object and get the id using transcrtip id. 
+/* 
+    //get request
+    while(true){
+        HttpRequest getRequest = HttpRequest.newBuilder()
+            .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+            .header("Authorization", "3dbe811b98354c81be4802a625657bc1")
+            .GET()
+            .build();
+
+        HttpResponse<String> getResponse = httpClient.send(getRequest,BodyHandlers.ofString());
+
+
+
+     }
+ 
+*/
         
         
 
--- backend-java/app/src/main/java/biz/readmylegal/backend/Transcript.java
+++ backend-java/app/src/main/java/biz/readmylegal/backend/Transcript.java
@@ -1,14 +1,57 @@
 package biz.readmylegal.backend;
 
-public class Transcript {
-    private String audio_url;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Transcript {
+
+    @JsonProperty("audio_url")
+    private String audio_url = "";
+    
+
+    @JsonProperty("id")
+    private String id = ""; 
+
+    @JsonProperty("status")
+    private String status = "";
+
+    @JsonProperty("text")
+    private String text = "";
+    
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    //setter and getter for audio
     public String getAudio_url() {
         return audio_url;
     }
 
     public void setAudio_url(String audio_url) {
         this.audio_url = audio_url;
+    }
+
+//setter for ID
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
 }
