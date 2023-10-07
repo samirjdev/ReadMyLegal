d81337416b3796b2c77398dcf4c44032d0180443
SsamDemon <sultan.samid@gmail.com>
2023-10-07 11:17:53 -0400

    Testing stage. I dont know what I pushed it,

--- backend-java/app/src/main/java/biz/readmylegal/backend/App.java
+++ backend-java/app/src/main/java/biz/readmylegal/backend/App.java
@@ -15,6 +15,7 @@ public class App {
     }
 
     public static void main(String[] args) {
-        GPTTest.test(args[0]);
+        //GPTTest.test(args[0]);
+        System.out.println("Hello World 3");
     }
 }
