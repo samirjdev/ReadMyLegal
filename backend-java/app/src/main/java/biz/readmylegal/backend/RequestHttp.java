9b461a4fbfe16681f0d90965469bb972779bdf38
SsamDemon <sultan.samid@gmail.com>
2023-10-07 20:14:10 -0400

    Everything is working. good job

--- backend-java/app/src/main/java/biz/readmylegal/backend/RequestHttp.java
+++ backend-java/app/src/main/java/biz/readmylegal/backend/RequestHttp.java
@@ -6,6 +6,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.lang.Thread;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -38,30 +39,41 @@ public class RequestHttp {
         HttpResponse<String> repostResponse = httpClient.send(postRequest,BodyHandlers.ofString());
 
         System.out.println("here is Jason Start "+ repostResponse.body());
+        
        //geting Id
         transcript = objectmapper.readValue(repostResponse.body(), Transcript.class);
 
-        System.out.println("Printing ID = "+transcript.getId());
+        //priting test
+        //System.out.println("Printing ID = "+ transcript.getId());
 
 
-    //get call.
     //covert jsyn into transcript object and get the id using transcrtip id. 
-/* 
+
     //get request
     while(true){
         HttpRequest getRequest = HttpRequest.newBuilder()
-            .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+            .uri(new URI("https://api.assemblyai.com/v2/transcript/" + transcript.getId() ))
             .header("Authorization", "3dbe811b98354c81be4802a625657bc1")
             .GET()
             .build();
 
         HttpResponse<String> getResponse = httpClient.send(getRequest,BodyHandlers.ofString());
 
+        transcript = objectmapper.readValue(getResponse.body(), Transcript.class);
 
+        System.out.println(transcript.getStatus());
 
+        if("completed".equals(transcript.getStatus() )|| "error".equals(transcript.getStatus())){
+            break;
+        }
+
+        Thread.sleep(1000);
      }
+
+     System.out.println("Transcription Completed ");
+     System.out.println(transcript.getText());
  
-*/
+
         
         
 
